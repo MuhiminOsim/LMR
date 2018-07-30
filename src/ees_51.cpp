4c9437883a38ed4c0b8d556a244f77d3c8428648
Nisiyama-Suzune <Nisiyama_Suzune@icloud.com>
2018-07-30 15:39:06 +0800

    ess

--- src/ees_51.cpp
+++ src/ees_51.cpp
@@ -1,24 +1,80 @@
-#include <bit/stdc++.h>
+#include <bits/stdc++.h>
 
-template <long long N = 2000000000000LL, int SN = 2000000, int D = 2>
+template <int SN = 110000, int D = 2>
 struct ees {
-	int co[SN + 1], prime[SN + 1], psize;
-	long long powa[SN + 1], powb[SN + 1];
-	ees () {
-		for (int i = 2; i <= SN; ++i) {
+	int co[SN], prime[SN], psize, sn;
+	long long powa[D + 1][SN], powb[D + 1][SN];
+	long long funca[SN], funcb[SN];
+	long long pow (long long x, int n) {
+		long long res = 1;
+		for (int i = 0; i < n; ++i) res *= x;
+		return res;
+	}
+	long long pre_pow (long long x, int n) {
+		if (n == 0) return x;
+		if (n == 1) return (1 + x) * x / 2;
+		if (n == 2) return (1 + 2 * x) * (1 + x) * x / 6;
+		return 0;
+	}
+	long long pfunc (long long p) { return -1; }
+	long long cfunc (long long k, long long p) { return 0; }
+	void assemble () {
+		for (int i = 1; i <= sn; ++i) {
+			funca[i] = -powa[0][i];
+			funcb[i] = -powb[0][i];
+		}
+	}
+	void init (long long n) {
+		sn = std::max ((int) (ceil (sqrt (n)) + 1), 2);
+		for (int i = 2; i <= sn; ++i) {
 			if (!co[i]) prime[psize++] = i;
-			for (int j = 0; 1LL * i * prime[j] <= SN; ++j) {
+			for (int j = 0; 1LL * i * prime[j] <= sn; ++j) {
 				co[i * prime[j]] = 1;
 				if (i % prime[j] == 0) break; } }
 		for (int d = 0; d <= D; ++d) {
-			for (int i = 1; i <= SN; ++i) powa[i] = pre_pow (i, d) - 1; 
-			for (int i = 1; i <= SN; ++i) powb[i] = pre_pow (N / i, d) - 1;
+			long long *pa = powa[d], *pb = powb[d];
+			for (int i = 1; i <= sn; ++i) pa[i] = pre_pow (i, d) - 1; 
+			for (int i = 1; i <= sn; ++i) pb[i] = pre_pow (n / i, d) - 1;
+			std::cout << d << " " << powb[0][1] << "\n";
 			for (int i = 0; i < psize; ++i) { int &pi = prime[i];
-				for (int j = 1; j <= SN; ++j) if (N / j >= pi * pi) {
-					long long ch = N / j / pi;
-					powb[j] -= ((ch <= SN ? powa[ch] : powb[j * pi]) - powa[pi - 1]) * pow (pi, d);
+				for (int j = 1; j <= sn; ++j) if (n / j >= 1LL * pi * pi) {
+					long long ch = n / j / pi;
+					pb[j] -= ((ch <= sn ? pa[ch] : pb[j * pi]) - pa[pi - 1]) * pow (pi, d);
+					std::cout << d << " " << pa[ch] << " " << pow (pi, d) << " " << powb[0][1] << "\n";
 				} else break;
-				for (int j = SN; j >= 1; --j) if (j >= pi * pi) {
+				for (int j = sn; j >= 1; --j) if (j >= 1LL * pi * pi)
+					pa[j] -= (pa[j / pi] - pa[pi - 1]) * pow (pi, d);
+				else break;
 			}
+		}
+		assemble ();
+	}
+	void dfs (int x, int f, long long mul, long long val, long long n, long long &res) {
+		if (x == psize) { 
+			std::cout << powb[0][1] << " " << funcb[mul] << " " << funca[f] << "\n";
+			if (n / mul > f) res += val * ((n / mul <= sn ? funca[n / mul] : funcb[mul]) - funca[f]);
+			if (f > 1 && mul % (f * f) == 0) res += val;
+			return;
+		}
+		dfs (x + 1, f, mul, val, n, res);
+		mul *= prime[x]; val = val * pfunc (prime[x]);
+		if (mul * prime[x] >= n) { mul *= prime[x]; val = cfunc (val, prime[x]); }
+		for (; mul <= n; mul *= prime[x], val = cfunc (val, prime[x])) {
+			dfs (x + 1, prime[x], mul, val, n, res);
+		}
+	}
+	long long solve (long long n) {
+		long long res = 1;
+		init (n); dfs (0, 1, 1, 1, n, res);
+		return res;
+	}
 };
+
+ees <> e;
+
+int main () {
+	long long A; std::cin >> A;
+	std::cout << e.solve (A) << std::endl;
+	return 0;
+}
 
