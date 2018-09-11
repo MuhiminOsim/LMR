d3c551ad61661b64b884b7e3fab97eae2fb160c4
Nisiyama-Suzune <Nisiyama_Suzune@icloud.com>
2018-09-10 20:31:50 -0400

    Test & fix

--- src/chordal-graph-zoj-1015.cpp
+++ src/chordal-graph-zoj-1015.cpp
@@ -1,12 +1,22 @@
+#include <bits/stdc++.h>
+
+template <int MAXN = 100000, int MAXM = 100000>
+struct edge_list {
+	int size, begin[MAXN], dest[MAXM], next[MAXM];
+	void clear (int n) { size = 0; std::fill (begin, begin + n, -1); }
+	edge_list (int n = MAXN) { clear (n); }
+	void add_edge (int u, int v) { dest[size] = v; next[size] = begin[u]; begin[u] = size++; } };
+
+struct point {
+	int lab, u;
+	point (int lab = 0, int u = 0) : lab (lab), u (u) {}
+	bool operator < (const point &a) const { return lab < a.lab; } };
+
 template <int MAXN = 100000, int MAXM = 100000>
 struct chordal_graph {
 	int n; edge_list <MAXN, MAXM> e;
 	int id[MAXN], seq[MAXN];
 	void init () {
-		struct point {
-			int lab, u;
-			point (int lab = 0, int u = 0) : lab (lab), u (u) {}
-			bool operator < (const point &a) const { return lab < a.lab; } };
 		std::fill (id, id + n, -1);
 		static int label[MAXN]; std::fill (label, label + n, 0);
 		std::priority_queue <point> q;
@@ -41,3 +51,21 @@ struct chordal_graph {
 			c[u] = k; res = std::max (res, k + 1);
 		}
 		return res; } };
+
+int N, M;
+chordal_graph <11000, 2100000> ch;
+
+int main () {
+	std::ios::sync_with_stdio (0);
+	while (std::cin >> N >> M && N) {
+		ch.n = N;
+		ch.e.clear (N);
+		for (int i = 0; i < M; ++i) {
+			int u, v; std::cin >> u >> v;
+			ch.e.add_edge (u - 1, v - 1);
+			ch.e.add_edge (v - 1, u - 1);
+		}
+		ch.init ();
+		std::cout << (ch.is_chordal () ? "Perfect\n\n" : "Imperfect\n\n");
+	}
+}
