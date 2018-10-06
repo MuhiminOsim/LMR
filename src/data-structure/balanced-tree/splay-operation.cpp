5531b2094f6874d3b6911b364b9b99b9c3c2abd6
Nisiyama-Suzune <Nisiyama_Suzune@icloud.com>
2018-10-06 13:09:22 -0400

    Fixed splay

--- src/data-structure/balanced-tree/splay-operation.cpp
+++ src/data-structure/balanced-tree/splay-operation.cpp
@@ -1,24 +1,19 @@
-void push_down (int x) {
-	if (~n[x].c[0]) push (n[x].c[0], n[x].t);
-	if (~n[x].c[1]) push (n[x].c[1], n[x].t);
-	n[x].t = tag (); }
-void update (int x) {
-	n[x].m = gen (x);
-	if (~n[x].c[0]) n[x].m = merge (n[n[x].c[0]].m, n[x].m);
-	if (~n[x].c[1]) n[x].m = merge (n[x].m, n[n[x].c[1]].m); }
 void rotate (int x, int k) {
-	int y = n[x].c[k]; n[x].c[k] = n[y].c[k ^ 1]; n[y].c[k ^ 1] = x;
-	if (n[x].f != -1) n[n[x].f].c[n[n[x].f].c[1] == x] = y;
-	n[y].f = n[x].f; n[x].f = y; if (~n[x].c[k]) n[n[x].c[k]].f = x; 
-	update (x); update (y); }
-void splay (int x, int s = -1) {
-	while (n[x].f != s) {
-		int a = -1; if (n[n[x].f].f != s) {
-			push_down (n[n[x].f].f);
-			a = n[n[n[x].f].f].c[1] == n[x].f; }
-		push_down (n[x].f); push_down (x);
-		int b = n[n[x].f].c[1] == x;
-		if (a == b) rotate (n[n[x].f].f, b);
-		else rotate (n[x].f, a);
-		if (n[x].f != s) rotate (n[x].f, n[n[x].f].c[1] == x); }
-	if (s == -1) root = x; }
+	int y=a[x].fa;
+	if (~a[y].fa) a[a[y].fa].son[a[a[y].fa].son[1]==y]=x;
+	a[x].fa=a[y].fa; a[y].son[k]=a[x].son[k^1];
+	if (~a[x].son[k^1]) a[a[x].son[k^1]].fa=y;
+	a[y].fa=x; a[x].son[k^1]=y;
+	update(y); update(x); }
+void splay(int x,int s=-1) {
+	push_down(x);
+	while (a[x].fa!=s) {
+		int y=a[x].fa,z=a[y].fa;
+		if (z==s) {
+			push_down(z=y); push_down(x);
+			rotate(x,a[y].son[1]==x);
+		} else {
+			push_down(z);push_down(y);push_down(x);
+			int t1=a[y].son[1]==x,t2=a[z].son[1]==y;
+			if (t1==t2)	rotate(y,t2),rotate(x,t1);
+			else rotate(x,t1),rotate(x,t2); } } }
