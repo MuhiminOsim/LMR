a9848705d0dec23b2ef8836fd9497e46dfb1e7ab
Nisiyama-Suzune <Nisiyama_Suzune@icloud.com>
2018-10-17 15:40:50 -0400

    exkmp

--- src/string/suffix/suffix-automaton.cpp
+++ src/string/suffix/suffix-automaton.cpp
@@ -1,30 +1,27 @@
 template <int MAXN = 1000000, int MAXC = 26>
 struct suffix_automaton {
 	struct state {
-		int len, right; state *parent, *dest[MAXC];
-		state (int len = 0, int right = 0) : len (len), right (right), parent (NULL) {
-			memset (dest, 0, sizeof (dest)); }
-	} node_pool[MAXN * 2], *tot_node, *null = new state();
-	state *head, *tail;
-	void extend (int token) {
-		state *p = tail;
-		state *np = tail -> dest[token] ? null : new (tot_node++) state (tail -> len + 1, tail -> len + 1);
-		while (p && !p -> dest[token]) p -> dest[token] = np, p = p -> parent;
-		if (!p) np -> parent = head;
+		int len, first, cnt; state *parent, *go[MAXC];
+		state (int len = 0, int first = 0, int cnt = 0) : len (len), first (first), cnt (cnt), parent (0) {
+			std::fill (go, go + MAXC, 0); } } node_pool[MAXN * 2], *tot_node, *start, *null;
+	state *extend (state *tail, int token) {
+		state *p = tail; state *np = tail -> go[token] ? null : new (*tot_node++) state (tail -> len + 1, tail -> len, 1);
+		while (p && !p -> go[token]) p -> go[token] = np, p = p -> parent;
+		if (!p) np -> parent = start;
 		else {
-			state *q = p -> dest[token];
+			state *q = p -> go[token];
 			if (p -> len + 1 == q -> len) {
-				np -> parent = q;
-			} else {
-				state *nq = new (tot_node++) state (*q);
-				nq -> len = p -> len + 1;
-				np -> parent = q -> parent = nq;
-				while (p && p -> dest[token] == q) {
-					p -> dest[token] = nq, p = p -> parent;
-				} } }
-		tail = np == null ? np -> parent : np; }
+				np -> parent = q; } else {
+				state *nq = new (tot_node++) state (*q); nq -> cnt = 0;
+				nq -> len = p -> len + 1; np -> parent = q -> parent = nq;
+				while (p && p -> go[token] == q) {
+					p -> go[token] = nq, p = p -> parent; } } }
+		return np == null ? np -> parent : np; }
+	void calc_cnt () {
+		static state *list[MAXN * 2]; state **end = list;
+		for (state *it = node_pool; it != tot_node; *(end++) = it++);
+		std::sort (list, end, [&] (state *a, state *b) { return a -> len > b -> len; });
+		for (state **it = list, it != end; ++it) (*it) -> parent -> cnt += (*it) -> cnt; }
 	void init () {
-		tot_node = node_pool;
-		head = tail = new (tot_node++) state (); }
+		tot_node = node_pool; start = new (tot_node++) state (); null = new state (); }
 	suffix_automaton () { init (); } };
-
